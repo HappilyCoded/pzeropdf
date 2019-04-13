cf2a4e9c9f1c40870fe6d1211093e510eb34aba8
Ajith Chandran <ajithredsmoke@gmail.com>
2019-04-13 12:12:24 +0200

    fixes broken scraper

--- src/Fetcher.ts
+++ src/Fetcher.ts
@@ -13,7 +13,7 @@ class Fetcher {
   public constructor(private _browser : Browser) {
 
   }
-  name(title): string {
+  name(title:string): string {
     return title.replace(/[^a-z0-9_-]/gi, '_').replace(/_{2,}/g, '_')
   }
 
@@ -33,22 +33,14 @@ class Fetcher {
   }
 
   async report(url : string) : Promise<void> {
-
     const page = await this._browser.newPage();
 
-    await page.goto(url, { waitUntil: 'load' });
-
-    const title = await page.evaluate(() => {
-      var d = document.getElementsByClassName("vt issueheader")[0]
-      const e = d.childNodes[1]
-      d.removeChild(e)
-      const title = d.getElementsByClassName("h3")[0]
-      return title.innerHTML
-    });
-
-    const content = await page.evaluate(() => {
-      return document.getElementsByClassName("vt issuedescription")[0].innerHTML
-    })
+    await page.goto(url);
+    
+    const title = await page.evaluate(() => document.querySelector('#right-part > div.issueheader > div:nth-child(2) > h1 > span').textContent);
+  
+    const contentDesc = await page.evaluate(() => document.querySelector('ezt-comments').getAttribute('description-list'))
+    const content = JSON.parse(contentDesc)[0]['content']
 
     const filename = this.name(title)
     Logger.log(filename)
@@ -80,11 +72,11 @@ class Fetcher {
     await page.pdf({ path: `issuepdfs/${filename}.pdf`, printBackground: true, displayHeaderFooter: false });
 
     await page.close()
+  
   }
 
   async finder(researcher: string = '' ) {
     const page = await this._browser.newPage();
-
     const url = `${this._host}/p/project-zero/issues/list?can=1&q=finder:${researcher}&colspec=ID+Summary&cells=ids&num=2000&start=1`
     await page.goto(url, { waitUntil: 'load' });
     let tdxpath = `//*[@id="resultstable"]/tbody/tr/td[2]`;
